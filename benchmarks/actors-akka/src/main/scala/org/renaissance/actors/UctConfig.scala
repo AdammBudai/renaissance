e08a3b65e2b2fb12ce6a51a5ba1202ae129acf12
budaia <adambudai3737@gmail.com>
2026-07-10 11:59:07 +0200

    scaling urgentPaths with thread count

--- benchmarks/actors-akka/src/main/scala/org/renaissance/actors/UctConfig.scala
+++ benchmarks/actors-akka/src/main/scala/org/renaissance/actors/UctConfig.scala
@@ -8,8 +8,15 @@ object UctConfig {
   /** Branching factor: each interior node has exactly this many children. */
   val BranchingFactor: Int = 10
 
-  /** Number of urgent paths (leaves) to select for the shock reduction. */
-  val UrgentPathCount: Int = 32
+  /** Number of urgent paths (leaves) to select for the shock reduction.
+   * We set bigger number of urgent paths than running threads due
+   * to a non-deterministic workload. With this approach we overload system and some
+   * actors will not be active some time. The goal is that when non-active actor
+   * wakes up, it sees 2 messages in its mailbox and dispatcher prioritize the urgent one.
+   * With lower or same urgent paths as threads it is possible that it will see only 1 (non-urgent)
+   *  message nad immediately starts to process it
+   */
+  val UrgentPathCount: Int = 2 * Runtime.getRuntime().availableProcessors()
 
   /** Minimum depth fraction [0, 1] a leaf must reach to be eligible for urgent path selection. */
   val UrgentLeafDepthFractionMin: Double = 0.9
